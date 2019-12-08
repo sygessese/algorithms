89d7b3d3205ab2ee2679dbb2730d9cd6536cfab3
Selam Gessese <sygessese@gmail.com>
2019-12-08 20:31:39 +0300

    reverse integer test disabled

--- reverseint/solution.test.js
+++ reverseint/solution.test.js
@@ -1,13 +1,13 @@
 const reverse = require('./solution');
 
-test('reverses positive numbers', () => {
+xtest('reverses positive numbers', () => {
   expect(reverse(123)).toBe(321);
 })
 
-test('reverses negative numbers', () => {
+xtest('reverses negative numbers', () => {
   expect(reverse(-123)).toBe(-321);
 })
 
-test(`accounts for leading 0's`, () => {
+xtest(`accounts for leading 0's`, () => {
   expect(reverse(120)).toBe(21);
 })
