48d754189643fa4f5f276e5ac0cda87c0da51801
Selam Gessese <sygessese@gmail.com>
2019-12-15 19:51:13 +0300

    solution

---
+++ haystack/solution.js
@@ -0,0 +1,18 @@
+function haystack(haystack, needle) {
+  if (needle === "") {
+    return 0;
+  }
+  if (haystack === needle) {
+    return 0;
+  }
+  for (var i = 0; i <= haystack.length - needle.length; i++) {
+    if (haystack[i] === needle[0]) {
+      if (haystack.slice(i, i + needle.length) === needle) {
+        return i
+      }
+    }
+  }
+  return -1;
+};
+
+module.exports = haystack;
