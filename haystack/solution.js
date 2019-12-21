ff4bbb4f1f7cd7524ce0ad1c56e23c9973646008
Selam Gessese <sygessese@gmail.com>
2019-12-21 02:20:44 -0800

    basecase modified

--- haystack/solution.js
+++ haystack/solution.js
@@ -2,9 +2,6 @@ function haystack(haystack, needle) {
   if (needle === "") {
     return 0;
   }
-  if (haystack === needle) {
-    return 0;
-  }
   for (var i = 0; i <= haystack.length - needle.length; i++) {
     if (haystack[i] === needle[0]) {
       if (haystack.slice(i, i + needle.length) === needle) {
