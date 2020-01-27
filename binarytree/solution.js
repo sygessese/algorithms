f5c2e58297d003ee29f3dcc90fc2878af7d72d06
Selam Gessese <sygessese@gmail.com>
2020-01-27 00:16:23 -0800

    equality check

--- binarytree/solution.js
+++ binarytree/solution.js
@@ -5,7 +5,6 @@ var symmetricTree = function (root, mirror = root) {
   if (root === null || mirror === null) {
     return false;
   }
-
   return symmetricTree(root.left, mirror.right) && symmetricTree(root.right, mirror.left) && (root.val === mirror.val)
 };
 
